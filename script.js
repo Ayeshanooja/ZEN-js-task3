2bd10f8d25c0ab65ade4d2a1210a8c7779741d99
Ayeshanooja <122464862+Ayeshanooja@users.noreply.github.com>
2023-01-13 14:00:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// QUESTION 1:
+// JSON ITERATE USING FOR LOOP
+ 
+let keys=['ayesha','nooja','sheeba','sowbia','divya']
+let values=[1,2,3,4,5]
+let person={}
+for(let i=0; i<keys.length;i++)
+{
+    person[keys[i]]=values[i]
+}
+// console.log(person);
+//output=>{ayesha: 1, nooja: 2, sheeba: 3, sowbia: 4, divya: 5}
+//         ayesha:1
+//         nooja:2
+//         sheeba:3
+//         sowbia:4
+//         divya:5
+
+//JSON ITERATE USING FOR IN
+
+const data={
+    name:'ayesha',
+    age :21,
+    sex :'female'
+}
+for(let keys in data)
+{
+    // console.log(keys+":" ,data[keys])
+}
+//output name: ayesha
+//       age: 21
+//        sex: female
+
+//JSON ITERATE USING FOREACH
+
+let fav=['video','book','java','year'];
+let items=['play','reader','script',2000];
+let solution={}
+fav.forEach((fav,i)=>{
+    solution[fav]=items[i]
+});
+// console.log(solution);
+//output=>{video: 'play', book: 'reader', java: 'script', year: 2000}//book: "reader
+//java: "script"
+//video: "play"
+//year: 2000
+
+//JSON ITERAITON USING FOR OF
+
+let cars={'ford':1,'toyota':2,'honda':3,'volkswagen':'fav'}
+let k= Object.keys(cars)
+for(let i of k)
+{
+    console.log(`${i} : ${cars[i]}`);
+}
+//output=>ford : 1
+//        toyota : 2
+//        honda : 3
+//        volkswagen : fav
+
+//QUESTION 2:
+
+//EASY RESUME DATA IN JSON FARMAT
+
+let resume={ 
+    name         :'ayesha nooja',
+    DOB          : '01.09.2000',
+    gender       :'female',
+    nationality  :'INDIAN',
+    maried_status:'single',
+    languages_known:['tamil','english','hindi','urdu'],
+    degree        :'BBA BANKING',
+    university_board:'Alagappa University',
+    percentage_CGPA : '78%',
+    year_of_passing :2022,
+    Experience : '3 years(part time)'
+
+}
+console.log(resume);
+//output=>{name: 'ayesha nooja', DOB: '01.09.2000', gender: 'female', nationality: 'INDIAN', maried_status: 'single', …}
+name            : "ayesha nooja"
+nationality     : "INDIAN"
+percentage_CGPA : "78%"
+university_board: "Alagappa University"
+year_of_passing : 2022
+DOB             : "01.09.2000"
+Experience      : "3 years(part time)"
+degree          : "BBA BANKING"
+gender          : "female"
+languages_known : Array(4)
+                 0: "tamil"
+                 1: "english"
+                 2: "hindi"
+                 3: "urdu"
+maried_status   : "single"
